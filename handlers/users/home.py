f094e45c95ac808ace3bcf31f148b7937bfdba4e
Murodali Narzullaev <murodalinarzullaevofficial@gmail.com>
2022-09-12 16:48:05 +0500

    fix bugs

--- handlers/users/home.py
+++ handlers/users/home.py
@@ -1,9 +1,11 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardRemove
 from loader import dp
 
 
 # Home
 @dp.message_handler(text='🔙 Orqaga qaytish', state='*')
-async def home(message: types.Message):
+async def home(message: types.Message, state: FSMContext):
+    await state.finish()
     await message.answer("Asosiy menuga qaytdingiz \nbotni qayta ishlatish uchun /start kmandasini ustiga bosing!", reply_markup=ReplyKeyboardRemove())
