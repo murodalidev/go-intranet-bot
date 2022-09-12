870f58899e02b91e2b97aa11c0d50e06032ed813
Murodali Narzullaev <murodalinarzullaevofficial@gmail.com>
2022-09-12 16:12:27 +0500

    fix bugs

--- handlers/users/home.py
+++ handlers/users/home.py
@@ -4,6 +4,6 @@ from loader import dp
 
 
 # Home
-@dp.message_handler(text='🔙 Orqaga qaytish', state=None)
+@dp.message_handler(text='🔙 Orqaga qaytish', state='*')
 async def home(message: types.Message):
     await message.answer("Asosiy menuga qaytdingiz \nbotni qayta ishlatish uchun /start kmandasini ustiga bosing!", reply_markup=ReplyKeyboardRemove())
