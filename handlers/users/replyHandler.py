86b1eb7f20569149110e5bf3f737c10ebe71c432
goazizbek <goazizbek@gmail.com>
2022-09-26 11:32:20 +0500

    socket messages -> bug fix

--- handlers/users/replyHandler.py
+++ handlers/users/replyHandler.py
@@ -60,7 +60,10 @@ async def send_message_via_socket(data):
         await websocket.recv()
 
 
-loop = asyncio.get_event_loop()
+async def run_socket(chat_id, new_msg):
+    asyncio.get_event_loop().run_until_complete(connect_socket())
+    asyncio.get_event_loop().run_until_complete(socket_chat_handshake(chat_id))
+    asyncio.get_event_loop().run_until_complete(send_message_via_socket(new_msg))
 
 
 @dp.message_handler(state=ReplyMessage.description)
@@ -118,11 +121,4 @@ async def write_description(msg: types.Message, state: FSMContext):
     # )
     await state.finish()
     await msg.answer("Xabaringiz qabul qilindi", reply_markup=homeKey)
-    # await socket_chat_handshake(chat_id)
-    # await send_message_via_socket(new_msg)
-    try:
-        await loop.run_until_complete(connect_socket())
-        await loop.run_until_complete(socket_chat_handshake(chat_id))
-        await loop.run_until_complete(send_message_via_socket(new_msg))
-    finally:
-        loop.close()
+    await run_socket(chat_id=chat_id, new_msg=new_msg)
